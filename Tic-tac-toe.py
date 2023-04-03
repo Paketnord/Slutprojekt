41947e57dde4eeea19b6cff9ab3383e8748fe3f2
Paketnord <125279413+Paketnord@users.noreply.github.com>
2023-04-03 15:36:41 +0200

    lade in brädet.

---
+++ Tic-tac-toe.py
@@ -0,0 +1,12 @@
+#3x3 bräde(visualisera)
+
+def board():
+    h=("\n _____ _____ ____")
+    v=("\n |    |    |    |") 
+    print(h,v,v,h,v,v,h,v,v,h)
+board()
+
+
+                    
+
+        
