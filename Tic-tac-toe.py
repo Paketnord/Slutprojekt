804de71168a43c0fb2758ace98a97982fd1cf5b2
Paketnord <125279413+Paketnord@users.noreply.github.com>
2023-04-25 09:56:56 +0200

    började lägga till lite saker för när man ska starta

--- Tic-tac-toe.py
+++ Tic-tac-toe.py
@@ -1,13 +1,23 @@
 #3x3 bräde(visualisera)
 def create_board(board):
-    print("  1  2  3")
-    print("1  |   | ")
+    print("  a  b  c")
+    print("1 /| / |/ ") 
     print(" ---------")
-    print("2  |   |  ")
+    print("2 /| / |/ ")
     print(" ---------")
-    print("3  |   |  ")
+    print("3 /| / |/ ")
+
+    choices = [A1,A2,A3,B1,B2,B3,C1,C2,C3]
+
+print("start game? Y/N")
+start_game = "Y"
+
+player1 = input()
+if player1 == start_game:
+    pass
+
+    
 
 
-                    
 
 
