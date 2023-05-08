f2c1a8146ced8ad95397bec60c72b52b5d0fd943
Paketnord <125279413+Paketnord@users.noreply.github.com>
2023-05-08 11:19:17 +0200

    jag la till så att man kan placera ut pjäser och började la till så en dator också kan göra det och har börjat gräja med omgångar.

--- Tic-tac-toe.py
+++ Tic-tac-toe.py
@@ -1,12 +1,16 @@
+
+import random
+
 board_choices = {"a1": "/", "b1": "/","c1" : "/","a2": "/", "b2": "/","c2": "/","a3": "/", "b3": "/", "c3": "/"}
 
 board_keys = []
 
 for key in board_choices:
     board_keys.append(key)
+turns = 0
 player1_c = []
+bot_options = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
 bot_c = []
-turns = []
     
 def board_create(board):
     print("  a b c")
@@ -24,7 +28,90 @@ print("start game? Y/N")
 if player1 == "Y":
     for i in range(1):
         board_create(board_choices)
-else:
+        
+print("player turn 1"):
+    turns += 1
+        
+player_choice = input()
+
+if player_choice == "a1":
+        board_choices["a1"] = "X"
+
+elif player_choice == "a2":
+    board_choices["a2"] = "x"
+    
+elif player_choice == "a3":
+    board_choices["a3"] = "x"
+    
+elif player_choice == "b1":
+    board_choices["b1"] = "x"
+    
+elif player_choice == "b2":
+    board_choices["b2"] = "x"
+    
+elif player_choice == "b3":
+    board_choices["b3"] = "x"
+    
+elif player_choice == "c1":
+    board_choices["c1"] = "x"
+    
+elif player_choice == "c2":
+    board_choices["c2"] = "x"
+    
+elif player_choice == "c3":
+    board_choices["c3"] = "x"
+    
+for i in range(1):
+    board_create(board_choices)
+    
+print("computer turn 1"):
+    turns +=1
+    
+random_choice = random.choice(bot_options)
+print(random_choice)
+
+if random_choice == "a1":
+    board_choices["a1"] = "o"
+    
+elif random_choice == "a2":
+    board_choices["a2"] = "o"
+    
+elif random_choice == "a3":
+    board_choices["a3"] = "o"
+
+elif random_choice == "b1":
+    board_choices["b1"] = "o"
+    
+elif random_choice == "b2":
+    board_choices["b2"] = "o"
+    
+elif random_choice == "b3":
+    board_choices["b3"] = "o"
+    
+elif random_choice == "c1":
+    board_choices["c1"] = "o"
+    
+elif random_choice == "c2":
+    board_choices["c2"] = "o"
+    
+elif random_choice == "c3":
+    board_choices["c3"] = "o"
+
+for i in range(1):
+    board_create(board_choices)
+    
+print("player turn 2"):
+    turns += 1
+
+player_choice = input()
+
+for i in range(1):
+    board_create(board_choices)
+    
+
+
+
+
     
     
 
